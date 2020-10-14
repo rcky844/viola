1bdd8b0efd508b323ee90b8f2133533d180789d7
Arturo Mejia <arturomejiamarmol@gmail.com>
2020-10-14 16:58:09 -0400

    java: UrlUtils: Allow filename to be optional in the ContentDisposition regex
    Signed-off-by: Ricky Cheung <rcheung844@gmail.com>
    Change-Id: I91c85f7fea47a72c1298eb9e89fbb10ce556420c

--- app/src/main/java/tipz/browservio/utils/UrlUtils.java
+++ app/src/main/java/tipz/browservio/utils/UrlUtils.java
@@ -121,14 +121,97 @@ public class UrlUtils {
     }
 
     /**
+     * This is the regular expression to match the content disposition type segment.
+     *
+     * A content disposition header can start either with inline or attachment followed by comma;
+     *  For example: attachment; filename="filename.jpg" or inline; filename="filename.jpg"
+     * (inline|attachment)\\s*; -> Match either inline or attachment, followed by zero o more
+     * optional whitespaces characters followed by a comma.
+     *
+     */
+    private static final String contentDispositionType = "(inline|attachment)\\s*;";
+
+    /**
+     * This is the regular expression to match filename* parameter segment.
+     *
+     * A content disposition header could have an optional filename* parameter,
+     * the difference between this parameter and the filename is that this uses
+     * the encoding defined in RFC 5987.
+     *
+     * Some examples:
+     *  filename*=utf-8''success.html
+     *  filename*=iso-8859-1'en'file%27%20%27name.jpg
+     *  filename*=utf-8'en'filename.jpg
+     *
+     * For matching this section we use:
+     * \\s*filename\\s*=\\s*= -> Zero or more optional whitespaces characters
+     * followed by filename followed by any zero or more whitespaces characters and the equal sign;
+     *
+     * (utf-8|iso-8859-1)-> Either utf-8 or iso-8859-1 encoding types.
+     *
+     * '[^']*'-> Zero or more characters that are inside of single quotes '' that are not single
+     * quote.
+     *
+     * (\S*) -> Zero or more characters that are not whitespaces. In this group,
+     * it's where we are going to have the filename.
+     *
+     */
+    private static final String contentDispositionFileNameAsterisk =
+            "\\s*filename\\*\\s*=\\s*(utf-8|iso-8859-1)'[^']*'(\\S*)";
+
+    /**
      * Format as defined in RFC 2616 and RFC 5987
      * Both inline and attachment types are supported.
+     * More details can be found
+     * https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Disposition
+     *
+     * The first segment is the [contentDispositionType], there you can find the documentation,
+     * Next, it's the filename segment, where we have a filename="filename.ext"
+     * For example, all of these could be possible in this section:
+     * filename="filename.jpg"
+     * filename="file\"name.jpg"
+     * filename="file\\name.jpg"
+     * filename="file\\\"name.jpg"
+     * filename=filename.jpg
+     *
+     * For matching this section we use:
+     * \\s*filename\\s*=\\s*= -> Zero or more whitespaces followed by filename followed
+     *  by zero or more whitespaces and the equal sign.
+     *
+     * As we want to extract the the content of filename="THIS", we use:
+     *
+     * \\s* -> Zero or more whitespaces
+     *
+     *  (\"((?:\\\\.|[^|"\\\\])*)\" -> A quotation mark, optional : or \\ or any character,
+     *  and any non quotation mark or \\\\ zero or more times.
+     *
+     *  For example: filename="file\\name.jpg", filename="file\"name.jpg" and filename="file\\\"name.jpg"
+     *
+     * We don't want to match after ; appears, For example filename="filename.jpg"; foo
+     * we only want to match before the semicolon, so we use. |[^;]*)
+     *
+     * \\s* ->  Zero or more whitespaces.
+     *
+     *  For supporting cases, where we have both filename and filename*, we use:
+     * "(?:;$contentDispositionFileNameAsterisk)?"
+     *
+     * Some examples:
+     *
+     * attachment; filename="_.jpg"; filename*=iso-8859-1'en'file%27%20%27name.jpg
+     * attachment; filename="_.jpg"; filename*=iso-8859-1'en'file%27%20%27name.jpg
      */
-    private static final Pattern CONTENT_DISPOSITION_PATTERN =
-            Pattern.compile("(inline|attachment)\\s*;" +
+    private static final Pattern contentDispositionPattern =
+            Pattern.compile(contentDispositionType +
                             "\\s*filename\\s*=\\s*(\"((?:\\\\.|[^\"\\\\])*)\"|[^;]*)\\s*" +
-                            "(?:;\\s*filename\\*\\s*=\\s*(utf-8|iso-8859-1)'[^']*'(\\S*))?",
+                            "(?:;" + contentDispositionFileNameAsterisk + ")?",
                     Pattern.CASE_INSENSITIVE);
+
+    /**
+     * This is an alternative content disposition pattern where only filename* is available
+     */
+    private static final Pattern fileNameAsteriskContentDispositionPattern =
+            Pattern.compile(contentDispositionType +
+                    contentDispositionFileNameAsterisk, Pattern.CASE_INSENSITIVE);
 
     /**
      * Keys for the capture groups inside CONTENT_DISPOSITION_PATTERN
@@ -138,34 +221,65 @@ public class UrlUtils {
     private static final int QUOTED_FILE_NAME_GROUP = 3;
     private static final int UNQUOTED_FILE_NAME = 2;
 
+    /**
+     * Belongs to the [fileNameAsteriskContentDispositionPattern]
+     */
+    private static final int ALTERNATIVE_FILE_NAME_GROUP = 3;
+    private static final int ALTERNATIVE_ENCODING_GROUP = 2;
+
     @Nullable
     private static String parseContentDisposition(String contentDisposition) {
         try {
-            Matcher m = CONTENT_DISPOSITION_PATTERN.matcher(contentDisposition);
-
-            if (m.find()) {
-                // If escaped string is found, decode it using the given encoding.
-                String encodedFileName = m.group(ENCODED_FILE_NAME_GROUP);
-                String encoding = m.group(ENCODING_GROUP);
-
-                if (encodedFileName != null && encoding != null) {
-                    return decodeHeaderField(encodedFileName, encoding);
-                }
-
-                // Return quoted string if available and replace escaped characters.
-                String quotedFileName = m.group(QUOTED_FILE_NAME_GROUP);
-
-                if (quotedFileName != null) {
-                    return quotedFileName.replaceAll("\\\\(.)", "$1");
-                }
-
-                // Otherwise try to extract the unquoted file name
-                return m.group(UNQUOTED_FILE_NAME);
-            }
-        } catch (IllegalStateException | UnsupportedEncodingException ex) {
+            String filename = parseContentDispositionWithFileName(contentDisposition);
+            if (filename == null)
+                return parseContentDispositionWithFileNameAsterisk(contentDisposition);
+            else
+                return filename;
+        } catch (IllegalStateException ex) {
             // This function is defined as returning null when it can't parse the header
         }
 
+        return null;
+    }
+
+    private static String parseContentDispositionWithFileName(String contentDisposition) {
+        Matcher m = contentDispositionPattern.matcher(contentDisposition);
+        if (m.find()) {
+            String encodedFileName = m.group(ENCODED_FILE_NAME_GROUP);
+            String encoding = m.group(ENCODING_GROUP);
+            if (encodedFileName != null && encoding != null) {
+                try {
+                    return decodeHeaderField(encodedFileName, encoding);
+                } catch (UnsupportedEncodingException e) {
+                    // Do nothing
+                }
+            } else {
+                // Return quoted string if available and replace escaped characters.
+                String quotedFileName = m.group(QUOTED_FILE_NAME_GROUP);
+                if (quotedFileName != null)
+                    return quotedFileName.replaceAll("\\\\(.)", "$1");
+                else
+                    return m.group(UNQUOTED_FILE_NAME);
+            }
+        }
+        return null;
+    }
+
+    private static String parseContentDispositionWithFileNameAsterisk(String contentDisposition) {
+        Matcher alternative = fileNameAsteriskContentDispositionPattern.matcher(contentDisposition);
+
+        if (alternative.find()) {
+            String encoding = alternative.group(ALTERNATIVE_ENCODING_GROUP);
+            String fileName = alternative.group(ALTERNATIVE_FILE_NAME_GROUP);
+            if (encoding == null || fileName == null)
+                return null;
+
+            try {
+                return decodeHeaderField(fileName, encoding);
+            } catch (UnsupportedEncodingException e) {
+                // Do nothing
+            }
+        }
         return null;
     }
 
@@ -235,6 +349,7 @@ public class UrlUtils {
         }
 
         if (extension == null) {
+            // checking startsWith to ignoring encoding value such as "text/html; charset=utf-8"
             if (mimeType != null && mimeType.toLowerCase(Locale.ROOT).startsWith("text/")) {
                 if (mimeType.equalsIgnoreCase("text/html")) {
                     extension = ".html";
