a625837a94032723d91552712e1e48ee2ffaf93b
Ricky Cheung <rcheung844@gmail.com>
2021-06-13 10:51:48 +0800

    UrlUtils: Allow anything after the domain name too
    Signed-off-by: Ricky Cheung <rcheung844@gmail.com>

--- app/src/main/java/tipz/browservio/Utils/UrlUtils.java
+++ app/src/main/java/tipz/browservio/Utils/UrlUtils.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 
 public class UrlUtils {
 
-    public static final String BASIC_URL_REGEX = "^((https?|ftp)://)?(www\\.)?([\\w]+\\.)+[\u200C\u200B\\w]{2,63}/?$";
+    public static final String BASIC_URL_REGEX = "^((https?|ftp)://)?(www\\.)?([\\w]+\\.)+[\u200C\u200B\\w]{2,63}/?.*$";
     public static Pattern p = Pattern.compile(BASIC_URL_REGEX);
     public static Matcher m;
 
