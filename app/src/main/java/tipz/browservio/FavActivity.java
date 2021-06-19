1cbf97adce362850d80c7cf614911fecd8e302eb
Ricky Cheung <rcheung844@gmail.com>
2021-06-19 08:07:20 +0800

    fav: java: Make a popup menu on long press
    Signed-off-by: Ricky Cheung <rcheung844@gmail.com>

--- app/src/main/java/tipz/browservio/FavActivity.java
+++ app/src/main/java/tipz/browservio/FavActivity.java
@@ -6,9 +6,11 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
 import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
+import android.widget.PopupMenu;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -37,6 +39,8 @@ public class FavActivity extends AppCompatActivity {
 	private SharedPreferences bookmarks;
 	private TimerTask populate;
 	private AlertDialog.Builder del_fav;
+
+	private Boolean popup;
 	@Override
 	protected void onCreate(Bundle _savedInstanceState) {
 		super.onCreate(_savedInstanceState);
@@ -63,25 +67,40 @@ public class FavActivity extends AppCompatActivity {
 		del_fav = new AlertDialog.Builder(this);
 		
 		listview.setOnItemClickListener((_param1, _param2, _param3, _param4) -> {
-			BrowservioSaverUtils.setPref(browservio_saver, "needLoad", "1");
-			BrowservioSaverUtils.setPref(browservio_saver, "needLoadUrl", BrowservioSaverUtils.getPref(bookmarks, "bookmark_".concat(String.valueOf((long)(_param3)))));
-			finish();
+			if (!popup) {
+				BrowservioSaverUtils.setPref(browservio_saver, "needLoad", "1");
+				BrowservioSaverUtils.setPref(browservio_saver, "needLoadUrl", BrowservioSaverUtils.getPref(bookmarks, "bookmark_".concat(String.valueOf((long)(_param3)))));
+				finish();
+			} else {
+				popup = false;
+			}
 		});
 		
 		listview.setOnItemLongClickListener((_param1, _param2, _param3, _param4) -> {
-			final int _position = _param3;
-			del_fav.setTitle(getResources().getString(R.string.del_fav_title));
-			del_fav.setMessage(getResources().getString(R.string.del_fav_title));
-			del_fav.setPositiveButton(android.R.string.yes, (_dialog, _which) -> {
-				bookmark_list.remove(_position);
-				BrowservioSaverUtils.setPref(bookmarks, "bookmark_".concat(String.valueOf((long)(_position))).concat("_show"), "0");
-				((BaseAdapter)listview.getAdapter()).notifyDataSetChanged();
-				BrowservioBasicUtil.showMessage(getApplicationContext(), getResources().getString(R.string.del_success));
-				isEmptyCheck(bookmark_list, bookmarks);
+			popup = true;
+			PopupMenu popup1 = new PopupMenu(FavActivity.this, _param2);
+			Menu menu1 = popup1.getMenu();
+			menu1.add(getResources().getString(R.string.del_fav));
+			popup1.setOnMenuItemClickListener(item -> {
+				if (item.getTitle().toString().equals(getResources().getString(R.string.del_hist))) {
+					final int _position = _param3;
+					del_fav.setTitle(getResources().getString(R.string.del_fav_title));
+					del_fav.setMessage(getResources().getString(R.string.del_fav_title));
+					del_fav.setPositiveButton(android.R.string.yes, (_dialog, _which) -> {
+						bookmark_list.remove(_position);
+						BrowservioSaverUtils.setPref(bookmarks, "bookmark_".concat(String.valueOf((long)(_position))).concat("_show"), "0");
+						((BaseAdapter)listview.getAdapter()).notifyDataSetChanged();
+						BrowservioBasicUtil.showMessage(getApplicationContext(), getResources().getString(R.string.del_success));
+						isEmptyCheck(bookmark_list, bookmarks);
+					});
+					del_fav.setNegativeButton(android.R.string.no, null);
+					del_fav.create().show();
+					return true;
+				}
+				return false;
 			});
-			del_fav.setNegativeButton(android.R.string.no, null);
-			del_fav.create().show();
-			return true;
+			popup1.show();
+			return false;
 		});
 		
 		_fab.setOnClickListener(_view -> {
