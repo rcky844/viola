6c20c384d8fd130068d535e229c1e3f9637a0705
Ricky Cheung <rcheung844@gmail.com>
2021-06-26 17:57:23 +0800

    UrlUtils: Stop using regex and use androidx nullable annotation instead
    Signed-off-by: Ricky Cheung <rcheung844@gmail.com>

--- app/src/main/java/tipz/browservio/utils/UrlUtils.java
+++ app/src/main/java/tipz/browservio/utils/UrlUtils.java
@@ -1,18 +1,11 @@
 package tipz.browservio.utils;
 
-import android.content.SharedPreferences;
+import android.webkit.URLUtil;
 
-import org.jetbrains.annotations.Nullable;
-
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import static tipz.browservio.utils.BrowservioSaverUtils.getPref;
+import androidx.annotation.Nullable;
 
 public class UrlUtils {
 
-    private static final String BASIC_URL_REGEX = "^((https?|ftp)://)?(www\\.)?([\\w]+\\.)+[\u200C\u200B\\w]{2,63}/?.*$";
-    private static final Pattern p = Pattern.compile(BASIC_URL_REGEX);
     private static final String[] startsWithMatch = {
             "http:", "https:", "ftp:", "file:",
             "about:", "javascript:", "blob:", "data:"};
@@ -24,26 +17,24 @@ public class UrlUtils {
      *
      * @param url is supplied as the URL to check.
      * @param canBeSearch sets if it should be changed to a search term when the supplied URL isn't valid.
-     * @param pref as the SharedPreference to get the default search engine from.
-     * @param defaultSearchPerf as the tag to get the default search engine from pref.
+     * @param searchUrl as the Url supplied for search.
      * @return result
      */
     @Nullable
-    public static String UrlChecker(String url, boolean canBeSearch, SharedPreferences pref, String defaultSearchPerf) {
-        Matcher m = p.matcher(url);
+    public static String UrlChecker(String url, boolean canBeSearch, String searchUrl) {
         for (String match : startsWithMatch) {
             if (url.startsWith(match)) {
                 return url;
             }
         }
-        if (m.find()) {
-            return "https://" + url;
+        if (URLUtil.isValidUrl(url)) {
+            return url;
         } else {
             if (url.endsWith("/") || url.endsWith("\\")) {
                 return "https://" + url;
             }
             if (canBeSearch) {
-                return getPref(pref, defaultSearchPerf) + url;
+                return searchUrl + url;
             }
         }
         return null;
