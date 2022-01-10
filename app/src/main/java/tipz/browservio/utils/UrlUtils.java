7c65b820ca787257cc4647643d3707210a48dc22
Ricky Cheung <rcheung844@gmail.com>
2022-01-10 20:54:59 +0800

    java: urlutils: Introduce a variant for non-search query
    Change-Id: I11db303b6ed7784da837c2a876c810812ecd82f9
    Signed-off-by: Ricky Cheung <rcheung844@gmail.com>

--- app/src/main/java/tipz/browservio/utils/UrlUtils.java
+++ app/src/main/java/tipz/browservio/utils/UrlUtils.java
@@ -8,6 +8,10 @@ public class UrlUtils {
             "http:", "https:", "ftp:", "file:",
             "about:", "javascript:", "blob:", "data:"};
 
+    public static String UrlChecker(String url) {
+        return UrlChecker(url, false, BrowservioBasicUtil.EMPTY_STRING);
+    }
+
     /**
      * URL Checker
      * <p>
