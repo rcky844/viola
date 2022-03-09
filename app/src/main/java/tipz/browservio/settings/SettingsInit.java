4e1aaf6b6f06ef45c46e03fc658f757a05eda308
Ricky Cheung <rcheung844@gmail.com>
2022-03-09 10:00:45 +0800

    java: settingsinit: Default to Baidu for the Chinese market
    Signed-off-by: Ricky Cheung <rcheung844@gmail.com>
    Change-Id: Ib6b26e38ef1195c1ba88f45393918e6a0bf8d1d8

--- app/src/main/java/tipz/browservio/settings/SettingsInit.java
+++ app/src/main/java/tipz/browservio/settings/SettingsInit.java
@@ -1,27 +1,37 @@
 package tipz.browservio.settings;
 
+import static tipz.browservio.settings.SettingsUtils.browservio_saver;
 import static tipz.browservio.utils.urls.SearchEngineEntries.getHomepageUrl;
 import static tipz.browservio.utils.urls.SearchEngineEntries.getSearchEngineUrl;
-import static tipz.browservio.settings.SettingsUtils.browservio_saver;
 
 import android.content.Context;
 
-import tipz.browservio.utils.urls.SearchEngineEntries;
 import tipz.browservio.utils.CommonUtils;
+import tipz.browservio.utils.urls.SearchEngineEntries;
 
 public class SettingsInit {
     public SettingsInit(Context mContext) {
+        boolean doesNotHaveGoogle = mContext.getResources().getConfiguration().locale.getCountry().equals("CN");
         boolean isEqualToOneFirstLaunch = CommonUtils.isIntStrOne(SettingsUtils.getPref(browservio_saver(mContext), SettingsKeys.isFirstLaunch));
         SettingsUtils.checkIfEmpty(browservio_saver(mContext), SettingsKeys.isJavaScriptEnabled, "1", isEqualToOneFirstLaunch);
         SettingsUtils.checkIfEmpty(browservio_saver(mContext), SettingsKeys.enableAdBlock, 0, isEqualToOneFirstLaunch);
-        SettingsUtils.checkIfEmpty(browservio_saver(mContext), SettingsKeys.defaultHomePage, getHomepageUrl(SearchEngineEntries.google), isEqualToOneFirstLaunch);
-        SettingsUtils.checkIfEmpty(browservio_saver(mContext), SettingsKeys.defaultHomePageId, 0, isEqualToOneFirstLaunch);
-        SettingsUtils.checkIfEmpty(browservio_saver(mContext), SettingsKeys.defaultSearch, getSearchEngineUrl(SearchEngineEntries.google, SearchEngineEntries.googleSearchSuffix), isEqualToOneFirstLaunch);
-        SettingsUtils.checkIfEmpty(browservio_saver(mContext), SettingsKeys.defaultSearchId, 0, isEqualToOneFirstLaunch);
         SettingsUtils.checkIfEmpty(browservio_saver(mContext), SettingsKeys.sendDNT, "0", isEqualToOneFirstLaunch);
         SettingsUtils.checkIfEmpty(browservio_saver(mContext), SettingsKeys.showFavicon, "1", isEqualToOneFirstLaunch);
-        SettingsUtils.checkIfEmpty(browservio_saver(mContext), SettingsKeys.enableSuggestions, 1, isEqualToOneFirstLaunch);
         SettingsUtils.checkIfEmpty(browservio_saver(mContext), SettingsKeys.updateTesting, 0, isEqualToOneFirstLaunch);
+
+        if (doesNotHaveGoogle) {
+            SettingsUtils.checkIfEmpty(browservio_saver(mContext), SettingsKeys.enableSuggestions, 0, isEqualToOneFirstLaunch);
+            SettingsUtils.checkIfEmpty(browservio_saver(mContext), SettingsKeys.defaultHomePage, getHomepageUrl(SearchEngineEntries.baidu), isEqualToOneFirstLaunch);
+            SettingsUtils.checkIfEmpty(browservio_saver(mContext), SettingsKeys.defaultHomePageId, 1, isEqualToOneFirstLaunch);
+            SettingsUtils.checkIfEmpty(browservio_saver(mContext), SettingsKeys.defaultSearch, getSearchEngineUrl(SearchEngineEntries.baidu, SearchEngineEntries.baiduSearchSuffix), isEqualToOneFirstLaunch);
+            SettingsUtils.checkIfEmpty(browservio_saver(mContext), SettingsKeys.defaultSearchId, 1, isEqualToOneFirstLaunch);
+        } else {
+            SettingsUtils.checkIfEmpty(browservio_saver(mContext), SettingsKeys.enableSuggestions, 1, isEqualToOneFirstLaunch);
+            SettingsUtils.checkIfEmpty(browservio_saver(mContext), SettingsKeys.defaultHomePage, getHomepageUrl(SearchEngineEntries.google), isEqualToOneFirstLaunch);
+            SettingsUtils.checkIfEmpty(browservio_saver(mContext), SettingsKeys.defaultHomePageId, 0, isEqualToOneFirstLaunch);
+            SettingsUtils.checkIfEmpty(browservio_saver(mContext), SettingsKeys.defaultSearch, getSearchEngineUrl(SearchEngineEntries.google, SearchEngineEntries.googleSearchSuffix), isEqualToOneFirstLaunch);
+            SettingsUtils.checkIfEmpty(browservio_saver(mContext), SettingsKeys.defaultSearchId, 0, isEqualToOneFirstLaunch);
+        }
         SettingsUtils.checkIfEmpty(browservio_saver(mContext), SettingsKeys.isFirstLaunch, "0", isEqualToOneFirstLaunch);
     }
 }
