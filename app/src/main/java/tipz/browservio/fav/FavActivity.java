be374f54bed0e50920fc5e341daff80cc01d433c
Ricky Cheung <rcheung844@gmail.com>
2022-04-09 09:31:55 +0800

    java: fav/hist: Merge Recycler and Activity
    Change-Id: I3afc5c44c227dab77778388886ca9735c0f2a140

--- app/src/main/java/tipz/browservio/fav/FavActivity.java
+++ app/src/main/java/tipz/browservio/fav/FavActivity.java
@@ -2,18 +2,26 @@ package tipz.browservio.fav;
 
 import static tipz.browservio.fav.FavApi.bookmarks;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ProgressBar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatTextView;
+import androidx.appcompat.widget.PopupMenu;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
@@ -23,6 +31,7 @@ import tipz.browservio.settings.SettingsUtils;
 import tipz.browservio.utils.CommonUtils;
 
 public class FavActivity extends AppCompatActivity {
+    private static List<String> listData;
     private ProgressBar PopulationProg;
 
     @Override
@@ -64,18 +73,17 @@ public class FavActivity extends AppCompatActivity {
         super.onStart();
         PopulationProg.setVisibility(View.VISIBLE);
         int populate_count = 0;
-        final List<String> bookmark_list = new ArrayList<>();
         boolean loopComplete = false;
         while (!loopComplete) {
             String shouldShow = SettingsUtils.getPref(bookmarks(FavActivity.this), SettingsKeys.bookmarked.concat(Integer.toString(populate_count)).concat(SettingsKeys.bookmarked_show));
             if (!shouldShow.equals("0")) {
                 if (shouldShow.isEmpty()) {
                     loopComplete = true;
-                    isEmptyCheck(bookmark_list, bookmarks(FavActivity.this));
+                    isEmptyCheck(listData, bookmarks(FavActivity.this));
                     PopulationProg.setVisibility(View.GONE);
                 } else {
                     String bookmarkTitle = SettingsKeys.bookmarked.concat(Integer.toString(populate_count)).concat(SettingsKeys.bookmarked_title);
-                    bookmark_list.add(SettingsUtils.getPref(bookmarks(FavActivity.this), bookmarkTitle).isEmpty() ?
+                    listData.add(SettingsUtils.getPref(bookmarks(FavActivity.this), bookmarkTitle).isEmpty() ?
                             SettingsUtils.getPref(bookmarks(FavActivity.this), SettingsKeys.bookmarked.concat(Integer.toString(populate_count))) :
                             SettingsUtils.getPref(bookmarks(FavActivity.this), bookmarkTitle));
                 }
@@ -83,7 +91,10 @@ public class FavActivity extends AppCompatActivity {
             populate_count++;
         }
 
-        new FavRecycler(this, this, findViewById(R.id.recyclerView), bookmark_list);
+        RecyclerView favList = findViewById(R.id.recyclerView);
+
+        favList.setLayoutManager(new LinearLayoutManager(this, RecyclerView.VERTICAL, false));
+        favList.setAdapter(new ItemsAdapter(this));
     }
 
     void isEmptyCheck(List<String> list, SharedPreferences out) {
@@ -92,6 +103,70 @@ public class FavActivity extends AppCompatActivity {
             out.edit().clear().apply();
             CommonUtils.showMessage(this, getResources().getString(R.string.fav_list_empty));
             finish();
+        }
+    }
+
+    public static class ItemsAdapter extends RecyclerView.Adapter<FavActivity.ItemsAdapter.ViewHolder> {
+        private final FavActivity mFavActivity;
+
+        static class ViewHolder extends RecyclerView.ViewHolder {
+            private final AppCompatTextView mTextView;
+
+            public ViewHolder(View view) {
+                super(view);
+                mTextView = view.findViewById(android.R.id.text1);
+            }
+        }
+
+        public ItemsAdapter(FavActivity favActivity) {
+            mFavActivity = favActivity;
+        }
+
+        @NonNull
+        @Override
+        public FavActivity.ItemsAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_list_item_1, parent, false);
+
+            return new FavActivity.ItemsAdapter.ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull FavActivity.ItemsAdapter.ViewHolder holder, int position) {
+            holder.mTextView.setText(listData.get(position));
+
+            holder.mTextView.setOnClickListener(view -> {
+                Intent needLoad = new Intent();
+                needLoad.putExtra("needLoadUrl", SettingsUtils.getPref(bookmarks(mFavActivity), SettingsKeys.bookmarked.concat(Integer.toString(position))));
+                mFavActivity.setResult(0, needLoad);
+                mFavActivity.finish();
+            });
+
+            holder.mTextView.setOnLongClickListener(view -> {
+                PopupMenu popup1 = new PopupMenu(mFavActivity, view);
+                Menu menu1 = popup1.getMenu();
+                menu1.add(mFavActivity.getResources().getString(R.string.del_fav));
+                menu1.add(mFavActivity.getResources().getString(android.R.string.copyUrl));
+                popup1.setOnMenuItemClickListener(item -> {
+                    if (item.getTitle().toString().equals(mFavActivity.getResources().getString(R.string.del_fav))) {
+                        listData.remove(position);
+                        SettingsUtils.setPref(bookmarks(mFavActivity), SettingsKeys.bookmarked.concat(String.valueOf(position)).concat(SettingsKeys.bookmarked_show), "0");
+                        notifyItemRangeRemoved(position, 1);
+                        mFavActivity.isEmptyCheck(listData, bookmarks(mFavActivity));
+                        return true;
+                    } else if (item.getTitle().toString().equals(mFavActivity.getResources().getString(android.R.string.copyUrl))) {
+                        CommonUtils.copyClipboard(mFavActivity, SettingsUtils.getPref(bookmarks(mFavActivity), SettingsKeys.bookmarked.concat(String.valueOf(position))));
+                        return true;
+                    }
+                    return false;
+                });
+                popup1.show();
+                return true;
+            });
+        }
+
+        @Override
+        public int getItemCount() {
+            return listData.size();
         }
     }
 }
--- app/src/main/java/tipz/browservio/history/HistoryActivity.java
+++ app/src/main/java/tipz/browservio/history/HistoryActivity.java
@@ -1,19 +1,37 @@
 package tipz.browservio.history;
 
-import android.os.Bundle;
+import static tipz.browservio.fav.FavApi.bookmarks;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatTextView;
+import androidx.appcompat.widget.PopupMenu;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 
 import tipz.browservio.R;
+import tipz.browservio.settings.SettingsKeys;
+import tipz.browservio.settings.SettingsUtils;
 import tipz.browservio.utils.CommonUtils;
 
 public class HistoryActivity extends AppCompatActivity {
+    private static List<String> listData;
 
     @Override
     protected void onCreate(Bundle _savedInstanceState) {
@@ -53,13 +71,94 @@ public class HistoryActivity extends AppCompatActivity {
         super.onStart();
         isEmptyCheck();
 
-        new HistoryRecycler(this, this, findViewById(R.id.recyclerView));
+
+        RecyclerView historyList = findViewById(R.id.recyclerView);
+        listData = new ArrayList<>(Arrays.asList(HistoryReader.history_data(this).trim().split("\n")));
+
+        historyList.setLayoutManager(new LinearLayoutManager(this, RecyclerView.VERTICAL, false));
+        historyList.setAdapter(new ItemsAdapter(this));
     }
 
     void isEmptyCheck() {
         if (HistoryReader.isEmptyCheck(this)) {
             CommonUtils.showMessage(this, getResources().getString(R.string.hist_empty));
             finish();
+        }
+    }
+
+    public static class ItemsAdapter extends RecyclerView.Adapter<HistoryActivity.ItemsAdapter.ViewHolder> {
+        private final HistoryActivity mHistoryActivity;
+
+        static class ViewHolder extends RecyclerView.ViewHolder {
+            private final AppCompatTextView mTextView;
+
+            public ViewHolder(View view) {
+                super(view);
+                mTextView = view.findViewById(android.R.id.text1);
+            }
+        }
+
+        public ItemsAdapter(HistoryActivity historyActivity) {
+            mHistoryActivity = historyActivity;
+        }
+
+        @NonNull
+        @Override
+        public HistoryActivity.ItemsAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_list_item_1, parent, false);
+
+            return new HistoryActivity.ItemsAdapter.ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull HistoryActivity.ItemsAdapter.ViewHolder holder, int position) {
+            holder.mTextView.setText(listData.get(position));
+
+            holder.mTextView.setOnClickListener(view -> {
+                Intent needLoad = new Intent();
+                needLoad.putExtra("needLoadUrl", listData.get(position));
+                mHistoryActivity.setResult(0, needLoad);
+                mHistoryActivity.finish();
+            });
+
+            holder.mTextView.setOnLongClickListener(view -> {
+                PopupMenu popup1 = new PopupMenu(mHistoryActivity, view);
+                Menu menu1 = popup1.getMenu();
+                menu1.add(mHistoryActivity.getResources().getString(R.string.del_hist));
+                menu1.add(mHistoryActivity.getResources().getString(android.R.string.copyUrl));
+                menu1.add(mHistoryActivity.getResources().getString(R.string.add_to_fav));
+                popup1.setOnMenuItemClickListener(item -> {
+                    if (item.getTitle().toString().equals(mHistoryActivity.getResources().getString(R.string.del_hist))) {
+                        listData.remove(position);
+                        StringBuilder out = new StringBuilder();
+                        for (Object o : listData) {
+                            out.append(o.toString());
+                            out.append(CommonUtils.LINE_SEPARATOR());
+                        }
+                        HistoryReader.write(mHistoryActivity, out.toString().trim());
+                        notifyItemRangeRemoved(position, 1);
+                        mHistoryActivity.isEmptyCheck();
+                        return true;
+                    } else if (item.getTitle().toString().equals(mHistoryActivity.getResources().getString(android.R.string.copyUrl))) {
+                        CommonUtils.copyClipboard(mHistoryActivity, listData.get(position));
+                        return true;
+                    } else if (item.getTitle().toString().equals(mHistoryActivity.getResources().getString(R.string.add_to_fav))) {
+                        SettingsUtils.setPref(bookmarks(mHistoryActivity), SettingsKeys.bookmarked_count, SettingsUtils.getPref(bookmarks(mHistoryActivity), SettingsKeys.bookmarked_count).isEmpty() ? "0" : String.valueOf((long) (Double.parseDouble(SettingsUtils.getPref(bookmarks(mHistoryActivity), SettingsKeys.bookmarked_count)) + 1)));
+                        SettingsUtils.setPref(bookmarks(mHistoryActivity), SettingsKeys.bookmarked.concat(SettingsUtils.getPref(bookmarks(mHistoryActivity), SettingsKeys.bookmarked_count)), listData.get(position));
+                        SettingsUtils.setPref(bookmarks(mHistoryActivity), SettingsKeys.bookmarked.concat(SettingsUtils.getPref(bookmarks(mHistoryActivity), SettingsKeys.bookmarked_count)).concat(SettingsKeys.bookmarked_show), "1");
+                        CommonUtils.showMessage(mHistoryActivity, mHistoryActivity.getResources().getString(R.string.saved_su));
+                        return true;
+                    }
+                    return false;
+                });
+                popup1.show();
+                return true;
+            });
+        }
+
+        @Override
+        public int getItemCount() {
+            return listData.size();
         }
     }
 }
