654f8c4a73c27ec265b33e1c30ae6630f03b1c86
Ricky Cheung <rcheung844@gmail.com>
2022-05-21 12:16:11 +0800

    java: settings: Switch to Baidu suggestions for chinese users
    Signed-off-by: Ricky Cheung <rcheung844@gmail.com>
    Change-Id: Ia4e5d1debef582d7c9150cfd669d975b1d59bf83

--- app/src/main/java/tipz/browservio/settings/SettingsInit.java
+++ app/src/main/java/tipz/browservio/settings/SettingsInit.java
@@ -14,7 +14,7 @@ public class SettingsInit {
         boolean isEqualToOneFirstLaunch = CommonUtils.isIntStrOne(SettingsUtils.getPref(browservio_saver(mContext), SettingsKeys.isFirstLaunch));
         SettingsUtils.checkIfEmpty(browservio_saver(mContext),
                 new String[]{SettingsKeys.defaultHomePage, SettingsKeys.defaultHomePageId, SettingsKeys.defaultSearch, SettingsKeys.defaultSearchId, SettingsKeys.defaultSuggestions, SettingsKeys.defaultSuggestionsId},
-                SettingsUtils.doesNotHaveGoogle ? new Object[]{getHomepageUrl(SearchEngineEntries.baidu), 1, getSearchEngineUrl(SearchEngineEntries.baidu, SearchEngineEntries.baiduSearchSuffix), 1, SearchEngineEntries.bingSearchSuggestionsUrl, 1}
+                SettingsUtils.doesNotHaveGoogle ? new Object[]{getHomepageUrl(SearchEngineEntries.baidu), 1, getSearchEngineUrl(SearchEngineEntries.baidu, SearchEngineEntries.baiduSearchSuffix), 1, SearchEngineEntries.baiduSearchSuggestionsUrl, 1}
                         : new Object[]{getHomepageUrl(SearchEngineEntries.google), 0, getSearchEngineUrl(SearchEngineEntries.google, SearchEngineEntries.googleSearchSuffix), 0, SearchEngineEntries.googleSearchSuggestionsUrl, 0}, isEqualToOneFirstLaunch);
         SettingsUtils.checkIfEmpty(browservio_saver(mContext),
                 new String[]{SettingsKeys.isJavaScriptEnabled, SettingsKeys.enableAdBlock, SettingsKeys.sendDNT, SettingsKeys.showFavicon, SettingsKeys.themeId, SettingsKeys.centerActionBar, SettingsKeys.updateTesting, SettingsKeys.isFirstLaunch},
