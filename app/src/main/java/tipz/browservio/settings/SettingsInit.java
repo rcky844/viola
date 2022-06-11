602df56de79267a789b3c689e1f4f7162ff0680b
Ricky Cheung <rcheung844@gmail.com>
2022-06-11 11:52:46 +0800

    java: settings: Use Google or Baidu for default for now
    Signed-off-by: Ricky Cheung <rcheung844@gmail.com>
    Change-Id: Id4c2cdde9b8a46e801b92860acdc8238f944ae1e

--- app/src/main/java/tipz/browservio/settings/SettingsInit.java
+++ app/src/main/java/tipz/browservio/settings/SettingsInit.java
@@ -1,6 +1,7 @@
 package tipz.browservio.settings;
 
 import static tipz.browservio.settings.SettingsUtils.browservio_saver;
+import static tipz.browservio.settings.SettingsUtils.doesNotHaveGoogle;
 import static tipz.browservio.utils.urls.SearchEngineEntries.getHomepageUrl;
 import static tipz.browservio.utils.urls.SearchEngineEntries.getSearchEngineUrl;
 
@@ -10,9 +11,12 @@ import tipz.browservio.utils.urls.SearchEngineEntries;
 
 public class SettingsInit {
     public SettingsInit(Context mContext) {
+        // TODO: Switch to Brave entirely
+        int searchSuggest = doesNotHaveGoogle ? 1 : 0;
+
         SettingsUtils.checkIfEmpty(browservio_saver(mContext),
                 new String[]{SettingsKeys.defaultHomePage, SettingsKeys.defaultHomePageId, SettingsKeys.defaultSearch, SettingsKeys.defaultSearchId, SettingsKeys.defaultSuggestions, SettingsKeys.defaultSuggestionsId},
-                new Object[]{getHomepageUrl(SearchEngineEntries.baseSearch[7]), 7, getSearchEngineUrl(SearchEngineEntries.baseSearch[7], SearchEngineEntries.searchSuffix[7]), 7, SearchEngineEntries.searchSuggestionsUrl[6], 6});
+                new Object[]{getHomepageUrl(SearchEngineEntries.baseSearch[7]), 7, getSearchEngineUrl(SearchEngineEntries.baseSearch[7], SearchEngineEntries.searchSuffix[7]), 7, SearchEngineEntries.searchSuggestionsUrl[searchSuggest], searchSuggest});
         SettingsUtils.checkIfEmpty(browservio_saver(mContext),
                 new String[]{SettingsKeys.isJavaScriptEnabled, SettingsKeys.enableAdBlock, SettingsKeys.sendDNT, SettingsKeys.showFavicon, SettingsKeys.themeId, SettingsKeys.centerActionBar, SettingsKeys.updateTesting},
                 new Object[]{"1", 0, "0", "1", 0, 1, 0});
