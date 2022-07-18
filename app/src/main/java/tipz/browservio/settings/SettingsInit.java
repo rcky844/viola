eacbcfc0c4d3297b9f41a1c91b5166ff17ff2b18
Ricky Cheung <rcheung844@gmail.com>
2022-07-18 19:10:40 +0800

    java: settings: Improve init
    Signed-off-by: Ricky Cheung <rcheung844@gmail.com>
    Change-Id: Icf8e3f0414ead223714660d97d0c48b4810b651b

--- app/src/main/java/tipz/browservio/settings/SettingsInit.java
+++ app/src/main/java/tipz/browservio/settings/SettingsInit.java
@@ -10,52 +10,54 @@ import android.content.SharedPreferences;
 import tipz.browservio.utils.urls.SearchEngineEntries;
 
 public class SettingsInit {
+    SharedPreferences pref;
     public SettingsInit(Context mContext) {
-        SharedPreferences pref = browservio_saver(mContext);
+        pref = browservio_saver(mContext);
 
         /* A bloopers fix for migrating from old versions */
         if (SettingsUtils.isFirstLaunch(pref) && SettingsUtils.getPrefNum(pref, SettingsKeys.protocolVersion) == 0
-            && !SettingsUtils.getPref(pref, SettingsKeys.isJavaScriptEnabled).isEmpty())
+                && !SettingsUtils.getPref(pref, SettingsKeys.isJavaScriptEnabled).isEmpty())
             SettingsUtils.setPref(pref, SettingsKeys.isFirstLaunch, "0");
 
-        if (!SettingsUtils.isFirstLaunch(pref)) {
-            switch (SettingsUtils.getPrefNum(pref, SettingsKeys.protocolVersion)) {
-                case 0:
-                    // TODO: finalize
-                    /* 2c15e330: java: urls: Add Brave Search */
-                    if (SettingsUtils.getPrefNum(pref, SettingsKeys.defaultHomePageId) == 7)
-                        SettingsUtils.setPrefNum(pref, SettingsKeys.defaultHomePageId, 8);
-                    if (SettingsUtils.getPrefNum(pref, SettingsKeys.defaultSearchId) == 7)
-                        SettingsUtils.setPrefNum(pref, SettingsKeys.defaultSearchId, 8);
-                    /* 8cdfc371: java: settings: Move most settings to integer and add protocol version */
-                    SettingsUtils.setPrefNum(pref, SettingsKeys.isJavaScriptEnabled,
-                            Integer.parseInt(SettingsUtils.getPref(pref, SettingsKeys.isJavaScriptEnabled)));
-                    SettingsUtils.setPrefNum(pref, SettingsKeys.sendDNT,
-                            Integer.parseInt(SettingsUtils.getPref(pref, SettingsKeys.sendDNT)));
-                    SettingsUtils.setPrefNum(pref, SettingsKeys.showFavicon,
-                            Integer.parseInt(SettingsUtils.getPref(pref, SettingsKeys.showFavicon)));
-                case 1:
-                    break;
-            }
+        if (SettingsUtils.isFirstLaunch(pref)) {
+            SettingsUtils.setPref(pref, SettingsKeys.defaultHomePage,
+                    getHomepageUrl(SearchEngineEntries.baseSearch[7]));
+            SettingsUtils.setPrefNum(pref, SettingsKeys.defaultHomePageId, 7);
+            SettingsUtils.setPref(pref, SettingsKeys.defaultSearch,
+                    getSearchEngineUrl(SearchEngineEntries.baseSearch[7],
+                            SearchEngineEntries.searchSuffix[7]));
+            SettingsUtils.setPrefNum(pref, SettingsKeys.defaultSearchId, 7);
+            SettingsUtils.setPref(pref, SettingsKeys.defaultSuggestions,
+                    SearchEngineEntries.searchSuggestionsUrl[6]);
+            SettingsUtils.setPrefNum(pref, SettingsKeys.defaultSuggestionsId, 6);
+            SettingsUtils.setPrefNum(pref, SettingsKeys.isJavaScriptEnabled, 1);
+            SettingsUtils.setPrefNum(pref, SettingsKeys.enableAdBlock, 0);
+            SettingsUtils.setPrefNum(pref, SettingsKeys.sendDNT, 0);
+            SettingsUtils.setPrefNum(pref, SettingsKeys.showFavicon, 1);
+            SettingsUtils.setPrefNum(pref, SettingsKeys.themeId, 0);
+            SettingsUtils.setPrefNum(pref, SettingsKeys.centerActionBar, 1);
+        } else {
+            protoVer0To1();
         }
         SettingsUtils.setPrefNum(pref, SettingsKeys.protocolVersion, 1); /* CURRENT_PROTOCOL_VERSION */
+        if (SettingsUtils.isFirstLaunch(pref))
+            SettingsUtils.setPref(pref, SettingsKeys.isFirstLaunch, "0");
+    }
 
-        SettingsUtils.checkIfEmpty(pref,
-                new String[]{SettingsKeys.defaultHomePage,
-                        SettingsKeys.defaultHomePageId,
-                        SettingsKeys.defaultSearch,
-                        SettingsKeys.defaultSearchId,
-                        SettingsKeys.defaultSuggestions,
-                        SettingsKeys.defaultSuggestionsId,
-                        SettingsKeys.isJavaScriptEnabled,
-                        SettingsKeys.enableAdBlock,
-                        SettingsKeys.sendDNT,
-                        SettingsKeys.showFavicon,
-                        SettingsKeys.themeId,
-                        SettingsKeys.centerActionBar},
-                new Object[]{getHomepageUrl(SearchEngineEntries.baseSearch[7]), 7,
-                        getSearchEngineUrl(SearchEngineEntries.baseSearch[7], SearchEngineEntries.searchSuffix[7]), 7,
-                        SearchEngineEntries.searchSuggestionsUrl[6], 6,
-                        1, 0, 0, 1, 0, 1});
+    private void protoVer0To1() {
+        if (SettingsUtils.getPrefNum(pref, SettingsKeys.protocolVersion) == 0) {
+            /* 2c15e330: java: urls: Add Brave Search */
+            if (SettingsUtils.getPrefNum(pref, SettingsKeys.defaultHomePageId) == 7)
+                SettingsUtils.setPrefNum(pref, SettingsKeys.defaultHomePageId, 8);
+            if (SettingsUtils.getPrefNum(pref, SettingsKeys.defaultSearchId) == 7)
+                SettingsUtils.setPrefNum(pref, SettingsKeys.defaultSearchId, 8);
+            /* 8cdfc371: java: settings: Move most settings to integer and add protocol version */
+            SettingsUtils.setPrefNum(pref, SettingsKeys.isJavaScriptEnabled,
+                    Integer.parseInt(SettingsUtils.getPref(pref, SettingsKeys.isJavaScriptEnabled)));
+            SettingsUtils.setPrefNum(pref, SettingsKeys.sendDNT,
+                    Integer.parseInt(SettingsUtils.getPref(pref, SettingsKeys.sendDNT)));
+            SettingsUtils.setPrefNum(pref, SettingsKeys.showFavicon,
+                    Integer.parseInt(SettingsUtils.getPref(pref, SettingsKeys.showFavicon)));
+        }
     }
 }
