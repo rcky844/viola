bd3ac2bf38b32dbb358a10012f46825af64ed51e
Ricky Cheung <rcheung844@gmail.com>
2022-07-27 15:07:50 +0800

    java: utils: download: Use substringBefore
    Signed-off-by: Ricky Cheung <rcheung844@gmail.com>
    Change-Id: I2321fc83c0f87ea9a2b694b322a8ecd75fbe2002

--- app/src/main/java/tipz/browservio/utils/DownloadUtils.java
+++ app/src/main/java/tipz/browservio/utils/DownloadUtils.java
@@ -71,7 +71,8 @@ public class DownloadUtils {
                 String dataInfo = url.substring(url.indexOf(":") + 1, url.indexOf(","));
                 String filename = System.currentTimeMillis() + "."
                         + MimeTypeMap.getSingleton().getExtensionFromMimeType(
-                                dataInfo.substring(0, dataInfo.contains(";") ? dataInfo.indexOf(";") : dataInfo.length()));
+                                StringUtils.substringBefore(dataInfo,
+                                        dataInfo.contains(";") ? dataInfo.indexOf(";") : dataInfo.length()));
                 File file = new File(path, filename);
                 try {
                     if (!path.exists())
