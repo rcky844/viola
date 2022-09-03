17f10d642684549e4094a9d62d770515aac25c8e
Ricky Cheung <rcheung844@gmail.com>
2022-09-03 20:14:18 +0800

    java: search: suggestion: Switch to using onUrlUpdated
    Signed-off-by: Ricky Cheung <rcheung844@gmail.com>
    Change-Id: I6eb199d52754b70d439a3b297f70781ce33b8054

--- app/src/main/java/tipz/browservio/search/SuggestionAdapter.java
+++ app/src/main/java/tipz/browservio/search/SuggestionAdapter.java
@@ -20,8 +20,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
-import tipz.browservio.webview.tabbies.BrowserActivity;
 import tipz.browservio.R;
+import tipz.browservio.webview.VioWebViewActivity;
 
 /*
     "Inspired" by LineageOS' Jelly
@@ -80,7 +80,7 @@ public class SuggestionAdapter extends ArrayAdapter<String> {
             title.setText(suggestion);
         }
 
-        copy_to_search_bar_button.setOnClickListener(view -> ((BrowserActivity) mContext).copyToSearchBar(suggestion));
+        copy_to_search_bar_button.setOnClickListener(view -> ((VioWebViewActivity) mContext).onUrlUpdated(suggestion));
         return convertView;
     }
 
