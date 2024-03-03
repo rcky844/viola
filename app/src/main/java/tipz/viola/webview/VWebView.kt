36c0006095484e2c8b4612820c51824b0317c2d0
Ricky Cheung <rcheung844@gmail.com>
2024-03-03 09:46:28 +0800

    VWebView: Split out WebViewClient

--- app/src/main/java/tipz/viola/webview/VWebView.kt
+++ app/src/main/java/tipz/viola/webview/VWebView.kt
@@ -20,15 +20,12 @@ package tipz.viola.webview
 import android.Manifest
 import android.annotation.SuppressLint
 import android.app.Activity
-import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.DialogInterface
-import android.content.Intent
 import android.content.pm.ActivityInfo
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.net.Uri
-import android.net.http.SslError
 import android.os.Build
 import android.os.Handler
 import android.util.AttributeSet
@@ -42,11 +39,8 @@ import android.webkit.CookieSyncManager
 import android.webkit.GeolocationPermissions
 import android.webkit.JsPromptResult
 import android.webkit.JsResult
-import android.webkit.RenderProcessGoneDetail
-import android.webkit.SslErrorHandler
 import android.webkit.ValueCallback
 import android.webkit.WebChromeClient
-import android.webkit.WebResourceResponse
 import android.webkit.WebSettings
 import android.webkit.WebView
 import android.widget.FrameLayout
@@ -60,7 +54,6 @@ import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
 import androidx.webkit.WebSettingsCompat
-import androidx.webkit.WebViewClientCompat
 import androidx.webkit.WebViewCompat
 import androidx.webkit.WebViewFeature
 import androidx.webkit.WebViewRenderProcess
@@ -81,9 +74,6 @@ import tipz.viola.utils.DownloadUtils
 import tipz.viola.utils.InternalUrls
 import tipz.viola.utils.UrlUtils
 import tipz.viola.webviewui.BaseActivity
-import java.io.ByteArrayInputStream
-import java.net.MalformedURLException
-import java.net.URL
 import java.util.Objects
 
 @SuppressLint("SetJavaScriptEnabled")
@@ -93,7 +83,6 @@ class VWebView(private val mContext: Context, attrs: AttributeSet?) : WebView(
     private var mVioWebViewActivity: VWebViewActivity? = null
     private val iconHashClient = (mContext.applicationContext as Application).iconHashClient!!
     private val webSettings = this.settings
-    private var adServersHandler: AdServersHandler
     private val mWebViewRenderProcess =
         if (WebViewFeature.isFeatureSupported(WebViewFeature.GET_WEB_VIEW_RENDERER)) WebViewCompat.getWebViewRenderProcess(
             this
@@ -128,6 +117,9 @@ class VWebView(private val mContext: Context, attrs: AttributeSet?) : WebView(
         return@Handler true
     }
 
+    public enum class PageLoadState {
+        PAGE_STARTED, PAGE_FINISHED, UPDATE_HISTORY, URL_UPDATE
+    }
 
     init {
         /* User agent init code */
@@ -146,7 +138,7 @@ class VWebView(private val mContext: Context, attrs: AttributeSet?) : WebView(
                 mContext, url!!, contentDisposition,
                 mimeType, currentUrl
             )
-            updateCurrentUrl(originalUrl)
+            onPageInformationUpdated(PageLoadState.URL_UPDATE, originalUrl!!, null)
             mVioWebViewActivity!!.onPageLoadProgressChanged(0)
             if (!canGoBack() && originalUrl == null && settingsPreference.getIntBool(SettingsKeys.closeAppAfterDownload))
                 mVioWebViewActivity!!.finish()
@@ -167,7 +159,7 @@ class VWebView(private val mContext: Context, attrs: AttributeSet?) : WebView(
 
         /* HTML5 API flags */webSettings.databaseEnabled = false
         webSettings.domStorageEnabled = true
-        this.webViewClient = WebClient()
+        this.webViewClient = VWebViewClient(mContext, this, AdServersHandler(settingsPreference))
         this.webChromeClient = ChromeWebClient()
         if (WebViewFeature.isFeatureSupported(WebViewFeature.WEB_VIEW_RENDERER_CLIENT_BASIC_USAGE) && mWebViewRenderProcess != null) WebViewCompat.setWebViewRenderProcessClient(
             this,
@@ -179,9 +171,6 @@ class VWebView(private val mContext: Context, attrs: AttributeSet?) : WebView(
             val message = titleHandler.obtainMessage()
             this.requestFocusNodeHref(message)
         }
-
-        /* Init Ad Servers Handler */
-        adServersHandler = AdServersHandler(settingsPreference)
     }
 
     @Suppress("deprecation")
@@ -269,7 +258,7 @@ class VWebView(private val mContext: Context, attrs: AttributeSet?) : WebView(
         }
 
         val checkedUrl = UrlUtils.toSearchOrValidUrl(mContext, url)
-        updateCurrentUrl(checkedUrl)
+        onPageInformationUpdated(PageLoadState.URL_UPDATE, checkedUrl, null)
 
         // Load URL
         super.loadUrl(checkedUrl, mRequestHeaders)
@@ -289,146 +278,36 @@ class VWebView(private val mContext: Context, attrs: AttributeSet?) : WebView(
         super.goForward()
     }
 
-    private fun updateCurrentUrl(url: String?) {
-        mVioWebViewActivity!!.onUrlUpdated(url)
+    fun onPageInformationUpdated(state : PageLoadState, url: String, favicon: Bitmap?) {
+        if (url == InternalUrls.aboutBlankUrl) return
         currentUrl = url
-    }
 
-    /**
-     * WebViewClient
-     */
-    inner class WebClient : WebViewClientCompat() {
-        private fun UrlSet(url: String) {
-            if (url == InternalUrls.aboutBlankUrl) {
-                currentUrl = null
-                return
+        when (state) {
+            PageLoadState.PAGE_STARTED -> {
+                mVioWebViewActivity!!.onFaviconProgressUpdated(true)
+                mVioWebViewActivity!!.onPageLoadProgressChanged(-1)
             }
-            if (currentUrl != url && urlShouldSet(url) || currentUrl == null) updateCurrentUrl(url)
-        }
-
-        override fun onPageStarted(view: WebView, url: String, icon: Bitmap?) {
-            UrlSet(url)
-            mVioWebViewActivity!!.onFaviconProgressUpdated(true)
-            mVioWebViewActivity!!.onFaviconUpdated(null, false)
-            mVioWebViewActivity!!.onDropDownDismissed()
-            mVioWebViewActivity!!.onPageLoadProgressChanged(-1)
-        }
-
-        override fun onPageFinished(view: WebView, url: String) {
-            if (view.originalUrl == null || view.originalUrl == url) doUpdateVisitedHistory(
-                view,
-                url,
-                true
-            )
-            mVioWebViewActivity!!.onFaviconProgressUpdated(false)
-            mVioWebViewActivity!!.onPageLoadProgressChanged(0)
-        }
-
-        override fun doUpdateVisitedHistory(view: WebView, url: String, isReload: Boolean) {
-            UrlSet(url)
-            if (updateHistory) {
-                if (currentUrl == null) return
-                currentBroha = Broha(title, currentUrl!!)
-                historyCommitted = false
+            PageLoadState.PAGE_FINISHED -> {
+                mVioWebViewActivity!!.onFaviconProgressUpdated(false)
+                mVioWebViewActivity!!.onPageLoadProgressChanged(0)
             }
-            if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.KITKAT_WATCH) CookieSyncManager.getInstance()
-                .sync() else CookieManager.getInstance().flush()
-            mVioWebViewActivity!!.onFaviconUpdated(null, true)
-            mVioWebViewActivity!!.onSwipeRefreshLayoutRefreshingUpdated(false)
-        }
-
-        @Deprecated("Deprecated in Java")
-        override fun onReceivedError(
-            view: WebView,
-            errorCode: Int,
-            description: String,
-            failingUrl: String
-        ) {
-            var errorContent = template
-            for (i in 0..5) errorContent = errorContent.replace(
-                "$$i",
-                mContext.resources.getStringArray(R.array.errMsg)[i]
-            )
-            errorContent = errorContent.replace("$6", description)
-
-            view.evaluateJavascript("""document.documentElement.innerHTML = `$errorContent`""", null)
-            view.stopLoading()
-        }
-
-        @Deprecated("Deprecated in Java")
-        override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
-            if (UrlUtils.isUriLaunchable(url)) return false
-            try {
-                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(UrlUtils.cve_2017_13274(url)))
-                mContext.startActivity(intent)
-            } catch (ignored: ActivityNotFoundException) {
-                CommonUtils.showMessage(
-                    mContext,
-                    resources.getString(R.string.toast_no_app_to_handle)
-                )
+            PageLoadState.UPDATE_HISTORY -> {
+                if (updateHistory && currentUrl != null) {
+                    currentBroha = Broha(title, currentUrl!!)
+                    historyCommitted = false
+                }
+                if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.KITKAT_WATCH) CookieSyncManager.getInstance()
+                    .sync() else CookieManager.getInstance().flush()
+                mVioWebViewActivity!!.onSwipeRefreshLayoutRefreshingUpdated(false)
             }
-            return true
-        }
 
-        @SuppressLint("WebViewClientOnReceivedSslError")
-        override fun onReceivedSslError(view: WebView, handler: SslErrorHandler, error: SslError) {
-            val dialog = MaterialAlertDialogBuilder(mContext)
-            var contentSummary = resources.getString(R.string.ssl_certificate_unknown)
-            when (error.primaryError) {
-                SslError.SSL_DATE_INVALID -> contentSummary =
-                    resources.getString(R.string.ssl_certificate_date_invalid)
-
-                SslError.SSL_INVALID -> contentSummary =
-                    resources.getString(R.string.ssl_certificate_invalid)
-
-                SslError.SSL_EXPIRED -> contentSummary =
-                    resources.getString(R.string.ssl_certificate_expired)
-
-                SslError.SSL_IDMISMATCH -> contentSummary =
-                    resources.getString(R.string.ssl_certificate_idmismatch)
-
-                SslError.SSL_NOTYETVALID -> contentSummary =
-                    resources.getString(R.string.ssl_certificate_notyetvalid)
-
-                SslError.SSL_UNTRUSTED -> contentSummary =
-                    resources.getString(R.string.ssl_certificate_untrusted)
+            PageLoadState.URL_UPDATE -> {
             }
-            dialog.setTitle(resources.getString(R.string.ssl_certificate_error_dialog_title))
-                .setMessage(
-                    resources.getString(
-                        R.string.ssl_certificate_error_dialog_content,
-                        contentSummary
-                    )
-                )
-                .setPositiveButton(resources.getString(android.R.string.ok)) { _: DialogInterface?, _: Int -> handler.proceed() }
-                .setNegativeButton(resources.getString(android.R.string.cancel)) { _: DialogInterface?, _: Int -> handler.cancel() }
-                .create().show()
         }
 
-        override fun onRenderProcessGone(view: WebView, detail: RenderProcessGoneDetail): Boolean {
-            return false
-        }
-
-        @Deprecated("Deprecated in Java")
-        override fun shouldInterceptRequest(view: WebView, url: String): WebResourceResponse? {
-            if (adServersHandler.adServers.isNullOrEmpty()) {
-                adServersHandler.downloadAdServers()
-                return super.shouldInterceptRequest(view, url)
-            }
-            try {
-                if (adServersHandler.adServers!!.contains(" " + URL(url).host) && settingsPreference.getInt(
-                        SettingsKeys.enableAdBlock
-                    ) == 1
-                )
-                    return WebResourceResponse(
-                        "text/plain", "utf-8",
-                        ByteArrayInputStream(CommonUtils.EMPTY_STRING.toByteArray())
-                    )
-            } catch (e: MalformedURLException) {
-                e.printStackTrace()
-            }
-            return super.shouldInterceptRequest(view, url)
-        }
+        mVioWebViewActivity!!.onUrlUpdated(url)
+        mVioWebViewActivity!!.onFaviconUpdated(favicon, false)
+        mVioWebViewActivity!!.onDropDownDismissed()
     }
 
     private fun setImmersiveMode(enable: Boolean) {
@@ -629,11 +508,6 @@ class VWebView(private val mContext: Context, attrs: AttributeSet?) : WebView(
         dialog.create().show()
     }
 
-
-    private fun urlShouldSet(url: String): Boolean {
-        return !(url == InternalUrls.aboutBlankUrl || url.startsWith(InternalUrls.prefix))
-    }
-
     fun setUA(
         view: AppCompatImageView?,
         enableDesktop: Boolean,
@@ -665,9 +539,5 @@ class VWebView(private val mContext: Context, attrs: AttributeSet?) : WebView(
     fun webViewReload() {
         if (currentUrl.isNullOrBlank()) return
         super.loadUrl(currentUrl!!)
-    }
-    companion object {
-        private const val template =
-            "<html>\n<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n<head>\n<title>$0</title>\n</head>\n<body>\n<div style=\"padding-left: 8vw; padding-top: 12vh;\">\n<div>\n<svg xmlns=\"http://www.w3.org/2000/svg\" height=\"96\" viewBox=\"0 -960 960 960\" width=\"96\" fill=\"currentColor\">\n<path d=\"M480.134-120q-74.673 0-140.41-28.339-65.737-28.34-114.365-76.922-48.627-48.582-76.993-114.257Q120-405.194 120-479.866q0-74.673 28.339-140.41 28.34-65.737 76.922-114.365 48.582-48.627 114.257-76.993Q405.194-840 479.866-840q74.673 0 140.41 28.339 65.737 28.34 114.365 76.922 48.627 48.582 76.993 114.257Q840-554.806 840-480.134q0 74.673-28.339 140.41-28.34 65.737-76.922 114.365-48.582 48.627-114.257 76.993Q554.806-120 480.134-120ZM440-162v-78q-33 0-56.5-23.5T360-320v-40L168-552q-3 18-5.5 36t-2.5 36q0 121 79.5 212T440-162Zm276-102q20-22 36-47.5t26.5-53q10.5-27.5 16-56.5t5.5-59q0-98.58-54.115-180.059Q691.769-741.538 600-777.538V-760q0 33-23.5 56.5T520-680h-80v80q0 17-11.5 28.5T400-560h-80v80h240q17 0 28.5 11.5T600-440v120h40q26 0 47 15.5t29 40.5Z\"/>\n</svg>\n</div>\n<div>\n<p style=\"font-family:sans-serif; font-weight: bold; font-size: 24px; margin-top: 24px; margin-bottom: 8px;\">$1</p>\n<p style=\"font-family:sans-serif; font-size: 16px; margin-top: 8px; margin-bottom: 24px;\">$2</p>\n<p style=\"font-family:sans-serif; font-weight: bold; font-size: 16px; margin-bottom: 8px;\">$3</p>\n<ul style=\"font-family:sans-serif; font-size: 16px; margin-top: 0px; margin-bottom: 0px;\">\n<li>$4</li>\n<li>$5</li>\n</ul>\n<p style=\"font-family:sans-serif; font-size: 12px; margin-bottom: 8px; color: #808080;\">$6</p>\n</div>\n</div>\n</body>\n</html>"
     }
 }
