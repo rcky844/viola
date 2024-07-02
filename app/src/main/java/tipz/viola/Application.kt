b587cf75101692eea4ff274f724f36091656270d
Ricky Cheung <rcheung844@gmail.com>
2024-07-02 13:30:10 +0800

    app: Fix multidex issue on KitKat
    Signed-off-by: Ricky Cheung <rcheung844@gmail.com>

--- app/src/main/java/tipz/viola/Application.kt
+++ app/src/main/java/tipz/viola/Application.kt
@@ -16,6 +16,8 @@
 package tipz.viola
 
 import android.app.Application
+import android.content.Context
+import androidx.multidex.MultiDex
 import com.google.android.material.color.DynamicColors
 import tipz.viola.download.DownloadClient
 import tipz.viola.settings.SettingsSharedPreference
@@ -31,5 +33,10 @@ class Application : Application() {
 
         // Observe dynamic colors changes
         DynamicColors.applyToActivitiesIfAvailable(this)
+    }
+
+    override fun attachBaseContext(base: Context?) {
+        super.attachBaseContext(base)
+        MultiDex.install(base)
     }
 }
