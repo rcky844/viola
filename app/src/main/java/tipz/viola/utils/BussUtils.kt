1e09104d9ed723f95b2ceeee983d7a18a6d889e3
Ricky Cheung <rcheung844@gmail.com>
2024-08-04 02:32:06 +0800

    utils: buss: added visual feedback during page load
    Signed-off-by: Ricky Cheung <rcheung844@gmail.com>

--- app/src/main/java/tipz/viola/utils/BussUtils.kt
+++ app/src/main/java/tipz/viola/utils/BussUtils.kt
@@ -10,6 +10,7 @@ import kotlinx.coroutines.launch
 import org.json.JSONObject
 import tipz.viola.download.MiniDownloadHelper
 import tipz.viola.webview.VWebView
+import tipz.viola.webview.VWebView.PageLoadState
 
 object BussUtils {
     const val LOG_TAG = "BussUtils"
@@ -22,6 +23,8 @@ object BussUtils {
         if (!url.startsWith(bussPrefix)) return false
         val split = url.replace(bussPrefix, "")
             .replace('.', '/')
+        view.onPageInformationUpdated(PageLoadState.PAGE_STARTED, url, null)
+        view.onPageLoadProgressChanged(20)
 
         CoroutineScope(Dispatchers.IO).launch {
             val data = MiniDownloadHelper.startDownload("${apiUrl}/domain/${split}")!!
@@ -32,13 +35,19 @@ object BussUtils {
                 "https://raw.githubusercontent.com/" +
                         "${ip.replace(githubPrefix, "")}/main/index.html"
             else ip
+            CoroutineScope(Dispatchers.Main).launch { view.onPageLoadProgressChanged(40) }
+
             val htmlData = MiniDownloadHelper.startDownload(realUrl)
+            CoroutineScope(Dispatchers.Main).launch { view.onPageLoadProgressChanged(60) }
 
             val parsedHtml = BussHtmlUtils.parseHtml(realUrl, htmlData)
+            CoroutineScope(Dispatchers.Main).launch { view.onPageLoadProgressChanged(80) }
             Log.d(LOG_TAG, parsedHtml)
+
             CoroutineScope(Dispatchers.Main).launch {
                 view.loadDataWithBaseURL(realUrl, parsedHtml,
                     "text/html", "UTF-8", url)
+                view.onPageInformationUpdated(PageLoadState.PAGE_FINISHED, url, null)
             }
         }
         return true
