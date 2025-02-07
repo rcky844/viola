2d6b96fe1b602b75601efcfad1cc526183f815e5
Ricky Cheung <rcheung844@gmail.com>
2025-02-08 00:06:52 +0800

    components: Introduce property display layout
    This small little view helps generate a list showing property
    and their respective value, which will be useful in displaying
    structured information.
    
    Change-Id: I4fd24e6b315d6dc91e761a3c9db72ee5f4a5ae6f
    Signed-off-by: Ricky Cheung <rcheung844@gmail.com>

---
+++ app/src/main/java/tipz/viola/widget/PropertyDisplayView.kt
@@ -0,0 +1,56 @@
+// Copyright (c) 2025 Tipz Team
+// SPDX-License-Identifier: Apache-2.0
+
+package tipz.viola.widget
+
+import android.content.Context
+import android.graphics.Typeface
+import android.util.AttributeSet
+import android.widget.TextView
+import androidx.appcompat.widget.LinearLayoutCompat
+import tipz.viola.ext.dpToPx
+
+class PropertyDisplayView : LinearLayoutCompat {
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attributeSet: AttributeSet) : super(context, attributeSet)
+
+    var property: ArrayList<Array<Any>> = arrayListOf(arrayOf(Any(), Any()))
+        get() = field
+        set(value) {
+            field = value
+            updateView()
+        }
+
+    private fun updateView() {
+        property.forEach {
+            val title = TextView(context).apply {
+                setPadding(0, context.dpToPx(8), 0, 0)
+                setTypeface(null, Typeface.BOLD)
+                isAllCaps = true
+            }
+            val value = TextView(context).apply {
+                setPadding(0, 0, 0, context.dpToPx(8))
+            }
+
+            if (it[0] is CharSequence) title.text = it[0] as CharSequence
+            else if (it[0] is Int) title.setText(it[0] as Int)
+            else return@forEach
+
+            var hasValue = true
+            if (it.size <= 1) {
+                hasValue = false
+            } else {
+                if (it[1] is CharSequence) value.text = it[1] as CharSequence
+                else if (it[1] is Int) value.setText(it[1] as Int)
+                else hasValue = false
+            }
+
+            addView(title)
+            if (hasValue) addView(value)
+        }
+    }
+
+    init {
+        orientation = VERTICAL
+    }
+}
