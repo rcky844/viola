6f59f705ba58c711cb4421fb9484c5cded8c8011
Ricky Cheung <rcheung844@gmail.com>
2025-02-08 18:12:45 +0800

    Viola 7.0 Beta 5 Patch 1
    Change-Id: I1fc24aa66f8f6d5fa0005611a2a6c3984e299d7f
    Signed-off-by: Ricky Cheung <rcheung844@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,9 +47,9 @@ android {
             "\"Apache License, Version 2.0\"")
         buildConfigField("String", "VERSION_CODENAME", "\"Exotic\"")
         buildConfigField("String", "VERSION_COPYRIGHT_YEAR", "\"2020-2025\"")
-        buildConfigField("String", "VERSION_BUILD_EXTRA", "\"Beta 5\"")
+        buildConfigField("String", "VERSION_BUILD_EXTRA", "\"Beta 5 Patch 1\"")
         buildConfigField("String", "VERSION_BUILD_ID", "\"${versionCode}\"")
-        buildConfigField("int", "VERSION_BUILD_REVISION", "0")
+        buildConfigField("int", "VERSION_BUILD_REVISION", "1")
         buildConfigField("String", "VERSION_BUILD_GIT_REVISION",
             "\"${getGitInfo("revision")}\"")
         buildConfigField("String", "VERSION_BUILD_BRANCH",
