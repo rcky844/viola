fe6eb660992aa904e7eecec2ca5dc3aad80968c7
Ricky Cheung <rcheung844@gmail.com>
2025-02-09 20:38:39 +0800

    webview: Wrap isEnabled setter for Main thread usage
    This was reverted in ffe04600f0a30526e815364c28d01f318a5d053d,
    but it caused warnings to return.
    
    Change-Id: I40b596da10236e516431841acb165fd398325792
    Signed-off-by: Ricky Cheung <rcheung844@gmail.com>

--- app/src/main/java/tipz/viola/webview/VSwipeRefreshLayout.kt
+++ app/src/main/java/tipz/viola/webview/VSwipeRefreshLayout.kt
@@ -8,6 +8,9 @@ import android.os.Build
 import android.util.AttributeSet
 import android.util.Log
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import tipz.viola.Application
 import tipz.viola.settings.SettingsKeys
 
@@ -49,7 +52,9 @@ class VSwipeRefreshLayout(
             if (!overscroll) Log.d(LOG_TAG, "Webpage does not want to overscroll.")
 
             layoutEnabled = overscroll
-            if (settingEnabled()) isEnabled = overscroll
+            CoroutineScope(Dispatchers.Main).launch {
+                if (settingEnabled()) isEnabled = overscroll
+            }
         }
     }
 }
