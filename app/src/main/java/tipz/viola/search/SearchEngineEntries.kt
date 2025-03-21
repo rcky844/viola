bb3468f8d3c3d83fa878d5214a1e9c1154b26fa3
Ricky Cheung <rcheung844@gmail.com>
2025-03-22 02:46:22 +0800

    search: Create engine items
    Change-Id: Ibb7ff40bb95d458eb8fab4f9fdb40549a7d83ce6
    Signed-off-by: Ricky Cheung <rcheung844@gmail.com>

--- app/src/main/java/tipz/viola/search/SearchEngineEntries.kt
+++ app/src/main/java/tipz/viola/search/SearchEngineEntries.kt
@@ -11,103 +11,75 @@ import java.util.Locale
 object SearchEngineEntries {
     private const val queryPlaceholder = "{query}"
     private const val languagePlaceholder = "{language}"
+
+    data class EngineItem(val name: String, val homePage: String? = null,
+                           val search: String? = null, val suggestion: String? = null)
     private val engines = arrayOf(
-        EngineObject().apply {
-            name = "google"
-            homePage = "https://www.google.com"
-            search = "$homePage/search?q=$queryPlaceholder"
+        EngineItem(name = "google", homePage = "https://www.google.com",
+            search = "https://www.google.com/search?q=$queryPlaceholder",
             suggestion = "https://www.google.com/complete/search?client=android&oe=utf8&ie=utf8&cp=4&xssi=t&gs_pcrt=undefined&hl=$languagePlaceholder&q=$queryPlaceholder"
-        },
-        EngineObject().apply {
-            name = "baidu"
-            homePage = "https://www.baidu.com"
-            search = "$homePage/s?wd=$queryPlaceholder"
+        ),
+        EngineItem(name = "baidu", homePage = "https://www.baidu.com",
+            search = "https://www.baidu.com/s?wd=$queryPlaceholder",
             suggestion = "https://suggestion.baidu.com/su?ie=UTF-8&wd=$queryPlaceholder&action=opensearch"
-        },
-        EngineObject().apply {
-            name = "duckduckgo"
-            homePage = "https://www.duckduckgo.com"
-            search = "$homePage/?q=$queryPlaceholder"
-            suggestion = "$homePage/ac/?q=$queryPlaceholder&type=list"
-        },
-        EngineObject().apply {
-            name = "bing"
-            homePage = "https://www.bing.com"
-            search = "$homePage/search?q=$queryPlaceholder"
+        ),
+        EngineItem(name = "duckduckgo", homePage = "https://www.duckduckgo.com",
+            search = "https://www.duckduckgo.com/?q=$queryPlaceholder",
+            suggestion = "https://www.duckduckgo.com/ac/?q=$queryPlaceholder&type=list"
+        ),
+        EngineItem(name = "bing", homePage = "https://www.bing.com",
+            search = "https://www.bing.com/search?q=$queryPlaceholder",
             suggestion = "https://api.bing.com/osjson.aspx?query=$queryPlaceholder&language=$languagePlaceholder"
-        },
-        EngineObject().apply {
-            name = "yahoo"
-            homePage = "https://search.yahoo.com"
-            search = "$homePage/search?p=$queryPlaceholder"
+        ),
+        EngineItem(name = "yahoo", homePage = "https://search.yahoo.com",
+            search = "https://search.yahoo.com/search?p=$queryPlaceholder",
             suggestion = "https://sugg.search.yahoo.net/sg/?output=fxjson&command=$queryPlaceholder"
-        },
-        EngineObject().apply {
-            name = "ecosia"
-            homePage = "https://www.ecosia.org"
-            search = "$homePage/search?q=$queryPlaceholder"
+        ),
+        EngineItem(name = "ecosia", homePage = "https://www.ecosia.org",
+            search = "https://www.ecosia.org/search?q=$queryPlaceholder",
             suggestion = "https://ac.ecosia.org/autocomplete?q=$queryPlaceholder&type=list"
-        },
-        EngineObject().apply {
-            name = "yandex"
-            homePage = "https://yandex.com"
-            search = "$homePage/search/?text=$queryPlaceholder"
-            suggestion = "$homePage/suggest/suggest-ya.cgi?v=4&part=$queryPlaceholder"
-        },
-        EngineObject().apply {
-            name = "brave"
-            homePage = "https://search.brave.com"
-            search = "$homePage/search?q=$queryPlaceholder"
-            suggestion = "$homePage/api/suggest?q=$queryPlaceholder"
-        },
-        EngineObject().apply {
-            name = "startpage"
-            homePage = "https://www.startpage.com"
-            search = "$homePage/do/search?query=$queryPlaceholder"
-            suggestion = "$homePage/suggestions?q=$queryPlaceholder"
-        },
-        EngineObject().apply {
-            name = "whoogle"
-            homePage = "https://whoogle.io"
-            search = "$homePage/search?q=$queryPlaceholder"
-            suggestion = "$homePage/autocomplete?q=$queryPlaceholder"
-        },
-        EngineObject().apply {
-            name = "swisscows"
-            homePage = "https://swisscows.com"
-            search = "$homePage/web?query=$queryPlaceholder"
+        ),
+        EngineItem(name = "yandex", homePage = "https://yandex.com",
+            search = "https://yandex.com/search/?text=$queryPlaceholder",
+            suggestion = "https://yandex.com/suggest/suggest-ya.cgi?v=4&part=$queryPlaceholder"
+        ),
+        EngineItem(name = "brave", homePage = "https://search.brave.com",
+            search = "https://search.brave.com/search?q=$queryPlaceholder",
+            suggestion = "https://search.brave.com/api/suggest?q=$queryPlaceholder"
+        ),
+        EngineItem(name = "startpage", homePage = "https://www.startpage.com",
+            search = "https://www.startpage.com/do/search?query=$queryPlaceholder",
+            suggestion = "https://www.startpage.com/suggestions?q=$queryPlaceholder"
+        ),
+        EngineItem(name = "whoogle", homePage = "https://whoogle.io",
+            search = "https://whoogle.io/search?q=$queryPlaceholder",
+            suggestion = "https://whoogle.io/autocomplete?q=$queryPlaceholder",
+        ),
+        EngineItem(name = "swisscows", homePage = "https://swisscows.com",
+            search = "https://swisscows.com/web?query=$queryPlaceholder",
             suggestion = "https://api.swisscows.com/suggest?query=$queryPlaceholder"
-        },
-        EngineObject().apply {
-            name = "qwant"
-            homePage = "https://www.qwant.com"
-            search = "$homePage/?q=$queryPlaceholder"
+        ),
+        EngineItem(name = "qwant", homePage = "https://www.qwant.com",
+            search = "https://www.qwant.com/?q=$queryPlaceholder",
             suggestion = "https://api.qwant.com/v3/suggest?q=$queryPlaceholder"
-        },
-        EngineObject().apply {
-            name = "sogou"
-            homePage = "https://www.sogou.com"
-            search = "$homePage/web?query=$queryPlaceholder"
+        ),
+        EngineItem(name = "sogou", homePage = "https://www.sogou.com",
+            search = "https://www.sogou.com/web?query=$queryPlaceholder",
             suggestion = "https://sor.html5.qq.com/api/getsug?key=$queryPlaceholder"
-        },
-        EngineObject().apply {
-            name = "so360"
-            homePage = "https://www.so.com"
-            search = "$homePage/s?q=$queryPlaceholder"
+        ),
+        EngineItem(name = "so360", homePage = "https://www.so.com",
+            search = "https://www.so.com/s?q=$queryPlaceholder",
             suggestion = "https://sug.so.360.cn/suggest?word=$queryPlaceholder"
-        },
-        EngineObject().apply {
-            name = "frogfind"
-            homePage = "http://frogfind.com"
-            search = "$homePage/?q=$queryPlaceholder"
-            suggestion = null
-        },
-        EngineObject() /* The object for custom URL */
+        ),
+        EngineItem(name = "frogfind", homePage = "http://frogfind.com",
+            search = "http://frogfind.com/?q=$queryPlaceholder",
+        ),
+        EngineItem(name = "") /* The object for custom URL */
     )
 
     val customIndex = engines.size - 1
 
-    private fun findObjWithName(name: String): EngineObject? = engines.find {
+    private fun findObjWithName(name: String): EngineItem? = engines.find {
         it.name == name
     }
 
