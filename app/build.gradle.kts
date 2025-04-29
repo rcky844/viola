39fb355399112defedb121ec7e3a6df90d067593
Ricky Cheung <rcheung844@gmail.com>
2025-04-30 04:46:28 +0800

    Set base versionCode to 100
    Change-Id: Iea8a271794a68843c45a577019b32387f7417bbe
    Signed-off-by: Ricky Cheung <rcheung844@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,7 +37,7 @@ android {
         applicationId = "tipz.viola"
         minSdk = 14
         targetSdk = 35
-        versionCode = 1
+        versionCode = 100
         versionName = "8.0"
 
         /* Extra build info */
