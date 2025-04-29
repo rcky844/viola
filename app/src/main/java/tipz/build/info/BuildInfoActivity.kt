b18a29cf3ed2621b1bb8c15b275abc656eeabf3a
Ricky Cheung <rcheung844@gmail.com>
2025-04-30 03:08:46 +0800

    buildinfo: Drop Exotic banner from source
    Change-Id: I387675a0ba99d5da4ce923d4974ed558d729002e
    Signed-off-by: Ricky Cheung <rcheung844@gmail.com>

--- app/src/main/java/tipz/build/info/BuildInfoActivity.kt
+++ app/src/main/java/tipz/build/info/BuildInfoActivity.kt
@@ -3,15 +3,9 @@
 
 package tipz.build.info
 
-import android.os.Build
 import android.os.Bundle
-import android.view.View
-import androidx.core.view.WindowInsetsCompat
-import androidx.core.view.updatePadding
 import tipz.viola.R
 import tipz.viola.databinding.ActivityBuildinfoBinding
-import tipz.viola.ext.doOnApplyWindowInsets
-import tipz.viola.ext.dpToPx
 import tipz.viola.webview.activity.BaseActivity
 
 class BuildInfoActivity : BaseActivity() {
@@ -29,34 +23,9 @@ class BuildInfoActivity : BaseActivity() {
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
         supportActionBar!!.setHomeButtonEnabled(true)
         toolbar.setNavigationOnClickListener { onBackPressedDispatcher.onBackPressed() }
-        toolbar.doOnApplyWindowInsets { v, insets, _, _ ->
-            insets.getInsets(WindowInsetsCompat.Type.systemBars()).apply {
-                v.updatePadding(top = top)
-                v.layoutParams.height = dpToPx(179) + top
-            }
-        }
 
         // Set-up preference
         supportFragmentManager.beginTransaction()
             .replace(R.id.list_container, BuildInfoFragment()).commit()
-    }
-
-    override fun doSettingsCheck() {
-        super.doSettingsCheck()
-
-        // Set light status bar
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            if (!isDarkMode(this)) {
-                // FIXME: Figure out why light status bar does not work on R+
-                windowInsetsController.hide(WindowInsetsCompat.Type.statusBars())
-            }
-        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            window.decorView.run {
-                // TODO: Investigate why setAppearanceLightStatusBars() does not work
-                @Suppress("DEPRECATION")
-                systemUiVisibility =
-                    systemUiVisibility and View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR.inv()
-            }
-        }
     }
 }
