rcky844/viola

commit 918d987bb944f7d84e99324ef17652497275602f

webview: Improve chrome and viola prefix handling
Change-Id: If50b0069d80e1e86cbd0b4c98abec9ab61328eae
Signed-off-by: Ricky Cheung <rcheung844@gmail.com>

app/src/main/java/tipz/viola/webview/VWebView.kt
@@ -51,7 +51,8 @@ import tipz.viola.database.instances.HistoryClient.UpdateHistoryState
 import tipz.viola.database.instances.IconHashClient
 import tipz.viola.download.DownloadClient
 import tipz.viola.download.database.Droha
-import tipz.viola.ext.equalsWithIgnore
+import tipz.viola.ext.Matcher
+import tipz.viola.ext.matchAndExec
 import tipz.viola.ext.showMessage
 import tipz.viola.search.SearchEngineEntries
 import tipz.viola.settings.SettingsKeys
@@ -344,50 +345,49 @@ class VWebView(private val context: Context, attrs: AttributeSet?) : WebView(
             val handlingSuffix = url
                 .replaceFirst(BrowserUrls.violaPrefix, "", true)
                 .replaceFirst(BrowserUrls.chromePrefix, "", true)
+                .substringBefore('/')
 
             // Browser mode specific URLs
-            activity.takeIf { it is BrowserActivity }?.let { it as BrowserActivity
-                // Bookmarks / Favorites
+            val act = activity as BrowserActivity?
+            val match = handlingSuffix.matchAndExec(listOf(
+                // Bookmarks
+                Matcher(BrowserUrls.bookmarksChromeSuffix, {
+                    act?.itemSelected(null, R.drawable.favorites)
+                }),
+
+                // Favorites
                 // "favorites" does not exist as "chrome://" prefix,
                 // so limit to "viola://" prefix.
-                if (handlingSuffix.equalsWithIgnore(BrowserUrls.bookmarksChromeSuffix)
-                    || (isViolaUrl && handlingSuffix.startsWith(BrowserUrls.favouritesViolaSuffix))) {
-                    it.itemSelected(null, R.drawable.favorites)
-                    return
-                }
+                Matcher(BrowserUrls.favouritesViolaSuffix, {
+                    if (isViolaUrl) act?.itemSelected(null, R.drawable.favorites)
+                }),
 
                 // History
-                if (handlingSuffix.equalsWithIgnore(BrowserUrls.historyChromeSuffix)) {
-                    it.itemSelected(null, R.drawable.history)
-                    return
-                }
+                Matcher(BrowserUrls.historyChromeSuffix, {
+                    act?.itemSelected(null, R.drawable.history)
+                }),
 
                 // New Tab
-                if (handlingSuffix.equalsWithIgnore(BrowserUrls.newTabChromeSuffix)) {
+                Matcher(BrowserUrls.newTabChromeSuffix, {
                     loadHomepage()
-                    return
-                }
+                }),
 
                 // New Tab Page
-                if (handlingSuffix.equalsWithIgnore(BrowserUrls.newTabPageChromeSuffix)) {
+                Matcher(BrowserUrls.newTabPageChromeSuffix, {
                     loadHomepage(true)
-                    return
-                }
+                }),
 
                 // New Tab Page (Third party)
-                if (handlingSuffix.equalsWithIgnore(BrowserUrls.newTabPageThirdPartyChromeSuffix)) {
+                Matcher(BrowserUrls.newTabPageChromeSuffix, {
                     loadHomepage(false)
-                    return
-                }
-            }
+                }),
 
-            // Quit
-            if (handlingSuffix.equalsWithIgnore(BrowserUrls.quitChromeSuffix)) {
-                activity.finish()
-                return
-            }
-
-            super.loadUrl("${BrowserUrls.chromePrefix}$handlingSuffix")
+                // Quit
+                Matcher(BrowserUrls.quitChromeSuffix, {
+                    act?.finish()
+                }),
+            ))
+            if (!match) super.loadUrl("${BrowserUrls.chromePrefix}$handlingSuffix")
 
             return
         }

app/src/main/java/tipz/viola/ext/String.kt
@@ -3,8 +3,13 @@
 
 package tipz.viola.ext
 
-fun String.equalsWithIgnore(other: String, ignoreStartIndex: Int = length): Boolean {
-    val b = substring(0, ignoreStartIndex - 1) == other
-    return if (b) true
-    else this == other
+data class Matcher(val name: String, val action: () -> Unit)
+fun String.matchAndExec(c: List<Matcher>): Boolean {
+    c.forEach {
+        if (this == it.name) {
+            it.action()
+            return true
+        }
+    }
+    return false
 }

commit 8ed307c375024bd9a7d770abcd88b39c73b6a48f

webview: Fix suffix usage for "new-tab-page-third-party"
This broke after 918d987b during move to Matcher.

Signed-off-by: Ricky Cheung <rcheung844@gmail.com>

app/src/main/java/tipz/viola/webview/VWebView.kt
@@ -384,7 +384,7 @@ class VWebView(private val context: Context, attrs: AttributeSet?) : WebView(
                 }),
 
                 // New Tab Page (Third party)
-                Matcher(BrowserUrls.newTabPageChromeSuffix, {
+                Matcher(BrowserUrls.newTabPageThirdPartyChromeSuffix, {
                     loadHomepage(false)
                 }),
 