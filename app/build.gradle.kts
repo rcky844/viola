bc54a95d6212591edea78d519ffac025c29ab572
Ricky Cheung <rcheung844@gmail.com>
2025-06-08 02:25:22 +0800

    ksp: Update modern appcompat to 1.7.1
    Bring in the minor bug fix.
    
    Change-Id: Ib83bcff72fb7735a8dacc2f2860cf679226f4db5
    Signed-off-by: Ricky Cheung <rcheung844@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -174,7 +174,7 @@ dependencies {
     // Modern dependencies
     val modernImplementation by configurations
     modernImplementation("androidx.core:core-ktx:1.16.0")
-    modernImplementation("androidx.appcompat:appcompat:1.7.0")
+    modernImplementation("androidx.appcompat:appcompat:1.7.1")
     modernImplementation("androidx.coordinatorlayout:coordinatorlayout:1.3.0")
     modernImplementation("com.google.android.material:material:1.12.0")
 
