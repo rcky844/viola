e7ddcd042b9e916c131a6505d32e7b03d59f355b
Ricky Cheung <rcheung844@gmail.com>
2025-06-08 13:48:59 +0800

    Viola 8.0
    Change-Id: I5e53b284903c3a98650631ad15679462c4e0dd09
    Signed-off-by: Ricky Cheung <rcheung844@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,7 +37,7 @@ android {
         applicationId = "tipz.viola"
         minSdk = 14
         targetSdk = 35
-        versionCode = 106
+        versionCode = 110
         versionName = "8.0"
 
         /* Extra build info */
