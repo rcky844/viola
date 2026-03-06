43db5ef8df16171aea21be07e62a0202a9749699
Ricky Cheung <rcheung844@gmail.com>
2026-03-06 20:05:57 +0800

    gradle: Multidex library is only required for pre-Android 5
    * Add stub function for modern flavour.
    
    Change-Id: If8764f282fef9b6dc2ab85dd90ff8321546dd02f
    Signed-off-by: Ricky Cheung <rcheung844@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -96,6 +96,7 @@ android {
         create("modern") {
             dimension = "apilevel"
             minSdk = 21
+
         }
         create("legacy") {
             dimension = "apilevel"
@@ -109,6 +110,11 @@ android {
     flavorDimensions.add("apilevel")
 
     sourceSets {
+        // Modern flavour
+        named("modern") {
+            java.srcDirs("src/modern")
+        }
+
         // Next release
         named("next") {
             res.srcDir("src/next/res")
@@ -146,7 +152,6 @@ dependencies {
     implementation("com.google.android.flexbox:flexbox:3.0.0")
 
     // AndroidX
-    implementation("androidx.multidex:multidex:2.0.1")
     implementation("androidx.preference:preference-ktx:1.2.1") {
         exclude(group = "androidx.lifecycle", module = "lifecycle-viewmodel")
         exclude(group = "androidx.lifecycle", module = "lifecycle-viewmodel-ktx")
@@ -180,6 +185,7 @@ dependencies {
     // These will remain on their currently defined versions indefinitely,
     // since these are the last known versions to support API 14+
     val legacyImplementation by configurations
+    legacyImplementation("androidx.multidex:multidex:2.0.1")
     legacyImplementation("androidx.core:core-ktx:1.12.0")
     legacyImplementation("androidx.appcompat:appcompat:1.6.1")
     legacyImplementation("androidx.coordinatorlayout:coordinatorlayout:1.2.0")
