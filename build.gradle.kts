dbb142289a07a27f3cd278b01cc2acbade4c9fea
Ricky Cheung <rcheung844@gmail.com>
2026-03-06 19:42:41 +0800

    gradle: Uprev to AGP 8.13.2 & Kotlin 2.0.21
    * This is the final version of usable plugins before
      the AGP 9 nightmare.
    
    Change-Id: Ibaee70580f7fa7ce44d6028589cec61912da95ab
    Signed-off-by: Ricky Cheung <rcheung844@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
-// Copyright (c) 2024 Tipz Team
+// Copyright (c) 2024-2026 Tipz Team
 // SPDX-License-Identifier: Apache-2.0
 
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.7.3" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
-    id("com.google.devtools.ksp") version "2.0.0-1.0.24" apply false
+    id("com.android.application") version "8.13.2" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+    id("com.google.devtools.ksp") version "2.0.21-1.0.28" apply false
 }
 
 buildscript {
@@ -14,8 +14,8 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.7.3")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
+        classpath("com.android.tools.build:gradle:8.13.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.21")
     }
 }
 
